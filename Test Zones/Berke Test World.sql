9808caadd9efed5dc260f1df232b2ced5190b233
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-03-26 04:43:38 -0400

    update 1

---
+++ Test Zones/Berke Test World.sql
@@ -0,0 +1,63 @@
+---------------------------------------------
+-- Berke Test World Made by hand by Laughing
+-- ----------------------------------------------------------------------------------------------
+-- TODO : Get world ID, area ID, DisplayInfo, OutfitInfo, QuestChecklistIdx?. Place X, Y, Z, RX.
+-- ----------------------------------------------------------------------------------------------
+SET @WORLD = 0;
+DELETE FROM `entity` WHERE `world` = @WORLD AND `area` IN (0);
+-- --------------------------------------
+-- Gold Ravenok - Race Test - BBO
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
+(@GUID+1, 0, 37696, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 10, 50);
+-- --------------------------------------
+-- Purple Ravenok - Race Test - BBO
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
+(@GUID+1, 0, 37697, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 10, 50);
+-- --------------------------------------
+-- Blue Ravenok - Race Test - BBO
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
+(@GUID+1, 0, 37698, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 10, 50);
+-- --------------------------------------
+-- Finish Line - Race Test - BBO
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
+(@GUID+1, 10, 37699, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 10, 50);
+-- --------------------------------------
+-- Race Manager - Race Test - BBO
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
+(@GUID+1, 10, 37700, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 10, 50);
+-- --------------------------------------
+-- Blue Ravenok - Player Vehicle - Race Test - BBO
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
+(@GUID+1, 3, 37701, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1056, 1056, 0);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 10, 50);
+-- --------------------------------------
+-- Purple Ravenok - Player Vehicle - Race Test - BBO
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
+(@GUID+1, 3, 37702, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1056, 1056, 0);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 10, 50);
