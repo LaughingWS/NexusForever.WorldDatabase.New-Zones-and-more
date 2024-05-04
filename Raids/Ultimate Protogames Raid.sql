e4c3b3fca8c8a7ccd6b28b23d2dbcad72ee78c89
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-05-04 17:57:40 -0400

    5/4/2024

--- Raids/Ultimate Protogames Raid.sql
+++ Raids/Ultimate Protogames Raid.sql
@@ -1,18 +1,19 @@
--- --------------------------------------
--- UP20 |is this the UProtoGames raid???|
--- --------------------------------------
--- Todo: Find skins, get Area, worldID. 
+-- -----------------------------------
+-- Ultimate Protogames Raid
+-- Made by hand, by Laughing
+-- -----------------------------------
+-- Todo: Get Area
 -- Need to place X, Y, Z, RX for all. 
 -- Get real HP (and shields if any)
--- --------------------------------------
-SET @WORLD = 0;
+-- -----------------------------------
+SET @WORLD = 3041;
 DELETE FROM `entity` WHERE `world` = @WORLD AND `area` IN (1);
 -- ---------------------------------------------
 -- Overcharger Boss
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
 INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 0, 34063, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+(@GUID+1, 0, 61420, @WORLD, 1, 0, 0, 0, 0, 0, 0, 36952, 10513, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 (@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
@@ -20,53 +21,48 @@ INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 -- Downsizer - Power Station 1
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 2, 34064, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 2, 61421, @WORLD, 1, 0, 0, 0, 0, 0, 0, 22604, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
 -- ---------------------------------------------
 -- Power Station 2
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 0, 34065, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 61422, @WORLD, 1, 0, 0, 0, 0, 0, 0, 22604, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
 -- ---------------------------------------------
 -- Power Station 3
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 0, 34066, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 61423, @WORLD, 1, 0, 0, 0, 0, 0, 0, 22604, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
 -- ---------------------------------------------
 -- Power Station 4
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 0, 34067, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 61424, @WORLD, 1, 0, 0, 0, 0, 0, 0, 22604, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
 -- ---------------------------------------------
 -- Platform
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 10, 36888, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 10, 65760, @WORLD, 1, 0, 0, 0, 0, 0, 0, 22862, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
 -- ---------------------------------------------
 -- Ball Lightning
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 0, 36891, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 65763, @WORLD, 1, 0, 0, 0, 0, 0, 0, 28701, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 (@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
@@ -74,26 +70,26 @@ INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 -- Invisble Tesla Coil Unit
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 0, 36898, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 682, 682);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 65771, @WORLD, 1, 0, 0, 0, 0, 0, 0, 22862, 682, 682);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
 -- ---------------------------------------------
 -- Protogames Door Unit - Default Closed
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 4, 37004, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 682, 682);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 4, 65891, @WORLD, 1, 0, 0, 0, 0, 0, 0, 23539, 682, 682);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
 -- ---------------------------------------------
 -- Spark
 -- ---------------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
-(@GUID+1, 0, 38513, @WORLD, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1322, 1322);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 68541, @WORLD, 1, 0, 0, 0, 0, 0, 0, 28701, 1322, 1322);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50),
 (@GUID+1, 0, 1);
+-- ---------------------------------------------------------
+-- here is some random IDs that may or may not be from here
+-- 61443, 61461, 
