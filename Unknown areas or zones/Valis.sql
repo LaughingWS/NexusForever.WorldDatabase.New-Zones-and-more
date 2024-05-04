d4497bb471f3c0e622f892dc66f2e22f321056b2
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-05-04 18:50:34 -0400

    5/4/24

--- Unknown areas or zones/Valis.sql
+++ Unknown areas or zones/Valis.sql
@@ -1,31 +1,32 @@
----------------------------------------
--- Valis Zone Made by hand by Laughing
--- ----------------------------------------------------------------------------------------------
--- TODO : Get world ID, area ID, DisplayInfo, OutfitInfo, QuestChecklistIdx?. Place X, Y, Z, RX.
--- ----------------------------------------------------------------------------------------------
+-- -------------------------------------
+-- Valis Zone 
+-- Made by hand by Laughing
+-- -------------------------------------------------------------------
+-- TODO: Get world ID, area ID, QuestChecklistIdx?. Place X, Y, Z, RX
+-- -------------------------------------------------------------------
 SET @WORLD = 0;
 DELETE FROM `entity` WHERE `world` = @WORLD AND `area` IN (0);
 -- --------------------------------------
--- [PH] Larralen Greatmaw - Valis - Quest Creature
+-- [PH] Larralen Greatmaw  - Quest Creature
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
 INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 1464, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+(@GUID+1, 0, 1464, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21424, 0, 0, 0, 0);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 (@GUID+1, 0, 1);
 -- --------------------------------------
--- [PH] Valis Sentinel - Valis - Creature
+-- [PH] Valis Sentinel - Creature
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
 INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 5800, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 170, 170);
+(@GUID+1, 0, 5800, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21309, 0, 170, 170, 0);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 (@GUID+1, 0, 1);
 -- --------------------------------------
--- [PH] Damaged Valis Sentinel - Valis - Phase 3 - Flavor
+-- [PH] Damaged Valis Sentinel - Phase 3 - Flavor
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
 INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 6899, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 170, 170);
+(@GUID+1, 0, 6899, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21309, 0, 170, 170, 0);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 (@GUID+1, 0, 1);
