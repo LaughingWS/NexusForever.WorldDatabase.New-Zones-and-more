7e8b2eef926d4c155cc1ddcba49b0a248016eba3
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-05-04 18:40:08 -0400

    5/4/24

--- Test Zones/Quest Test Island.sql
+++ Test Zones/Quest Test Island.sql
@@ -1,96 +1,82 @@
----------------------------------------------------
--- Quest Test Island zone Made by hand by Laughing
--- ----------------------------------------------------------------------------------------------
--- TODO : Get world ID, area ID, DisplayInfo, OutfitInfo, QuestChecklistIdx?. Place X, Y, Z, RX.
--- Errors: can't find map Questtestisland
--- ----------------------------------------------------------------------------------------------
+-- --------------------------------------
+-- Quest Test Island Zone
+-- Made by hand by Laughing
+-- --------------------------------------
+-- TODO: Get Area ID & Place X, Y, Z, RX
+-- --------------------------------------
 SET @WORLD = 625;
 DELETE FROM `entity` WHERE `world` = @WORLD AND `area` IN (0);
 -- --------------------------------------
 -- Realm First Kill Achievement - SWC
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 58414, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 196, 196, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 58414, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21335, 196, 196);
 -- --------------------------------------
 -- Realm First Guild Kill Achievement - SWC
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 58415, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 196, 196, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 58415, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21335, 196, 196);
 -- --------------------------------------
 -- Realm First Guild Kill Achievement - Flag Test - SWC
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 58472, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 196, 196, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 58472, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21335, 196, 196);
 -- --------------------------------------
 -- Rep Reward Quest Giver - SWC
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 10, 59937, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 171, 171, 0);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 10, 59937, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21350, 7480, 171, 171);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+(@GUID+1, 0, 1);
 -- --------------------------------------
 -- Example Interact - MRB
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 59978, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1236, 1236, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 59978, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21342, 7478, 1236, 1236);
 -- --------------------------------------
 -- Example Interact - MRB
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 59979, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1236, 1236, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 59979, @WORLD, 0, 0, 0, 0, 0, 0, 0, 24611, 1236, 1236);
 -- --------------------------------------
 -- [DNT] Mount Despawn Test Case [DNT] - SWC
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 3, 62474, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 171, 171, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 3, 62474, @WORLD, 0, 0, 0, 0, 0, 0, 0, 26520, 171, 171);
 -- --------------------------------------
 -- [TEST]  Re-scan Test
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 20, 65223, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 20, 65223, @WORLD, 0, 0, 0, 0, 0, 0, 0, 29737, 219, 219);
 -- --------------------------------------
 -- Weekly Quest Giver - Quest Test
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 10, 66068, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 171, 171, 0);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 10, 66068, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21350, 7480, 171, 171);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+(@GUID+1, 0, 1);
 -- --------------------------------------
 -- Monthly Quest Giver - Quest Test
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 10, 66069, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 171, 171, 0);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 10, 66069, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21350, 7480, 171, 171);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+(@GUID+1, 0, 1);
 -- --------------------------------------
 -- Yearly Quest Giver - Quest Test
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 10, 66070, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 171, 171, 0);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 10, 66070, @WORLD, 0, 0, 0, 0, 0, 0, 0, 21350, 7480, 171, 171);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+(@GUID+1, 0, 1);
