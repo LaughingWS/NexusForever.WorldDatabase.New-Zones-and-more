2cff993042f9686991a7b15ad5aeadb5e9497127
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-05-04 18:38:40 -0400

    5/4/24

--- Test Zones/Public Quest Island.sql
+++ Test Zones/Public Quest Island.sql
@@ -1,31 +1,30 @@
------------------------------------------------------
--- Public Quest Island Zone Made by hand by Laughing
--- ----------------------------------------------------------------------------------------------
--- TODO : Get world ID, area ID, DisplayInfo, OutfitInfo, QuestChecklistIdx?. Place X, Y, Z, RX.
--- ----------------------------------------------------------------------------------------------
+-- --------------------------------------
+-- Public Quest Island Zone
+-- Made by hand by Laughing
+-- ------------------------------------------------
+-- TODO: Get World ID, Area ID & Place X, Y, Z, RX
+-- ------------------------------------------------
 SET @WORLD = 0;
 DELETE FROM `entity` WHERE `world` = @WORLD AND `area` IN (0);
 -- --------------------------------------
--- PQI Questgiver
+-- Questgiver
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 28291, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 28291, @WORLD, 0, 0, 0, 0, 0, 0, 0, 23222, 7959, 219, 219);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+(@GUID+1, 0, 1);
 -- --------------------------------------
--- PQI Test Settler Depot
+-- Test Settler Depot
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 10, 28292, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
-INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 10, 28292, @WORLD, 0, 0, 0, 0, 0, 0, 0, 23966, 219, 219);
 -- --------------------------------------
--- PQI Vendor
+-- Vendor
 -- --------------------------------------
 SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
-INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`, `QuestChecklistIdx`) VALUES
-(@GUID+1, 0, 28293, @WORLD, 0, 0, 0, 0, 0, 0, 0, 0, 0, 219, 219, 0);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 28293, @WORLD, 0, 0, 0, 0, 0, 0, 0, 23222, 7959, 219, 219);
 INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
-(@GUID+1, 10, 50);
+(@GUID+1, 0, 1);
