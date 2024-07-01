2060d689ec0882021c4103ea0c9b56b7f35336e8
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-07-01 15:08:01 -0400

    Event Rework

---
+++ Events/Starfall/Starfall week 4.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------
+-- Starfall week 4
+-- Made by hand by Laughing
+-- --------------------------------------
+-- ---------------------------------
+-- Emperor Myrcalus (week 4)
+-- ---------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 74961, 22, 1,    1,      1,   1,  1,  0, 0, 24316, 0, 219, 219);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1,  0,     1),
+(@GUID+1, 10,    50);
+-- ---------------------------------
+-- Dorian Walker (week 4)
+-- ---------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 74965, 51, 1,    1,      1,   1,  1,  0, 0, 25510, 8952, 219, 219);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1,  0,     1),
+(@GUID+1, 10,    50);
