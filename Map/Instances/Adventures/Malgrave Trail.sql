f1ee160324be6a5db74ef2d12bcc8e9a4acce4ca
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-07-27 23:25:22 -0400

    7/27/24

---
+++ Map/Instances/Adventures/Malgrave Trail.sql
@@ -0,0 +1,6 @@
+-- --------------------------------------
+-- Malgrave Trail Adventure
+-- --------------------------------------
+DELETE FROM map_entrance WHERE mapId = 1181;
+INSERT INTO map_entrance (mapId, team, worldLocationId) VALUES
+(1181, 0, 13662);
