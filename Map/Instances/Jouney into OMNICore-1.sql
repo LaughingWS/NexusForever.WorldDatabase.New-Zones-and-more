ab3330dce6e3bd7049416f17b4c08e375f08d5d2
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-08-03 00:43:43 -0400

    8/3/24

---
+++ Map/Instances/Jouney into OMNICore-1.sql
@@ -0,0 +1,6 @@
+-- --------------------------------------
+-- Jouney into OMNICore-1
+-- --------------------------------------
+DELETE FROM map_entrance WHERE mapId = 3045;
+INSERT INTO map_entrance (mapId, team, worldLocationId) VALUES
+(3045, 0, 45947);
