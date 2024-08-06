ed2fb0b4127257bf3861dbeac02a4609e05858ce
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-08-06 02:41:08 -0400

    8/6/24

--- Events/Dungeon Chase.sql
+++ Events/Dungeon Chase.sql
@@ -35,6 +35,67 @@ INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
 (@GUID+1, 10,    50),
 (@GUID+2,  0, 60500),
 (@GUID+2, 10,    50);
+INSERT INTO `entity_vendor` (`Id`, `BuyPriceMultiplier`, `SellPriceMultiplier`) VALUES
+(@GUID+1, 1, 1),
+(@GUID+2, 1, 1);
+INSERT INTO `entity_vendor_category` (`Id`, `Index`, `LocalisedTextID`) VALUES
+(@GUID+1, 1, 750234),
+(@GUID+1, 2, 750246),
+(@GUID+1, 3, 750245),
+(@GUID+1, 4, 750247),
+(@GUID+2, 1, 750234),
+(@GUID+2, 2, 750246),
+(@GUID+2, 3, 750245),
+(@GUID+2, 4, 750247);
+INSERT INTO `entity_vendor_item` (`Id`, `Index`, `CategoryIndex`, `ItemId`, `ExtraCost1ItemOrCurrencyId`, `ExtraCost1Quantity`, `ExtraCost1Type`, `ExtraCost2ItemOrCurrencyId`, `ExtraCost2Quantity`, `ExtraCost2Type`) VALUES
+(@GUID+1,  1, 1, 86896, 86873,  40, 1, 0, 0, 0),
+(@GUID+1,  2, 1, 91391, 86873,  40, 1, 0, 0, 0),
+(@GUID+1,  3, 2, 85949, 86873,  20, 1, 0, 0, 0),
+(@GUID+1,  4, 2, 85961, 86873,  20, 1, 0, 0, 0),
+(@GUID+1,  5, 2, 85962, 86873,  20, 1, 0, 0, 0),
+(@GUID+1,  6, 2, 85945, 86873,  40, 1, 0, 0, 0),
+(@GUID+1,  7, 2, 85948, 86873,  40, 1, 0, 0, 0),
+(@GUID+1,  8, 2, 21785, 86873,  60, 1, 0, 0, 0),
+(@GUID+1,  9, 2, 85947, 86873,  60, 1, 0, 0, 0),
+(@GUID+1, 10, 3, 74673, 86873,  20, 1, 0, 0, 0),
+(@GUID+1, 11, 3, 85932, 86873,  20, 1, 0, 0, 0),
+(@GUID+1, 12, 3, 85933, 86873,  20, 1, 0, 0, 0),
+(@GUID+1, 13, 3, 85934, 86873,  20, 1, 0, 0, 0),
+(@GUID+1, 14, 3, 21670, 86873,  40, 1, 0, 0, 0),
+(@GUID+1, 15, 3, 85931, 86873,  40, 1, 0, 0, 0),
+(@GUID+1, 16, 3, 46516, 86873,  60, 1, 0, 0, 0),
+(@GUID+1, 17, 4, 85922, 86873,  20, 1, 0, 0, 0),
+(@GUID+1, 18, 4, 85923, 86873,  20, 1, 0, 0, 0),
+(@GUID+1, 19, 4, 85924, 86873,  20, 1, 0, 0, 0),
+(@GUID+1, 20, 4, 85925, 86873,  40, 1, 0, 0, 0),
+(@GUID+1, 21, 4, 85926, 86873,  40, 1, 0, 0, 0),
+(@GUID+1, 22, 4, 85927, 86873,  40, 1, 0, 0, 0),
+(@GUID+1, 23, 4, 85928, 86873,  60, 1, 0, 0, 0),
+(@GUID+1, 24, 4, 86901, 86873, 120, 1, 0, 0, 0),
+(@GUID+2,  1, 1, 86896, 86873,  40, 1, 0, 0, 0),
+(@GUID+2,  2, 1, 91391, 86873,  40, 1, 0, 0, 0),
+(@GUID+2,  3, 2, 85949, 86873,  20, 1, 0, 0, 0),
+(@GUID+2,  4, 2, 85961, 86873,  20, 1, 0, 0, 0),
+(@GUID+2,  5, 2, 85962, 86873,  20, 1, 0, 0, 0),
+(@GUID+2,  6, 2, 85945, 86873,  40, 1, 0, 0, 0),
+(@GUID+2,  7, 2, 85948, 86873,  40, 1, 0, 0, 0),
+(@GUID+2,  8, 2, 21785, 86873,  60, 1, 0, 0, 0),
+(@GUID+2,  9, 2, 85947, 86873,  60, 1, 0, 0, 0),
+(@GUID+2, 10, 3, 74673, 86873,  20, 1, 0, 0, 0),
+(@GUID+2, 11, 3, 85932, 86873,  20, 1, 0, 0, 0),
+(@GUID+2, 12, 3, 85933, 86873,  20, 1, 0, 0, 0),
+(@GUID+2, 13, 3, 85934, 86873,  20, 1, 0, 0, 0),
+(@GUID+2, 14, 3, 21670, 86873,  40, 1, 0, 0, 0),
+(@GUID+2, 15, 3, 85931, 86873,  40, 1, 0, 0, 0),
+(@GUID+2, 16, 3, 46516, 86873,  60, 1, 0, 0, 0),
+(@GUID+2, 17, 4, 85922, 86873,  20, 1, 0, 0, 0),
+(@GUID+2, 18, 4, 85923, 86873,  20, 1, 0, 0, 0),
+(@GUID+2, 19, 4, 85924, 86873,  20, 1, 0, 0, 0),
+(@GUID+2, 20, 4, 85925, 86873,  40, 1, 0, 0, 0),
+(@GUID+2, 21, 4, 85926, 86873,  40, 1, 0, 0, 0),
+(@GUID+2, 22, 4, 85927, 86873,  40, 1, 0, 0, 0),
+(@GUID+2, 23, 4, 85928, 86873,  60, 1, 0, 0, 0),
+(@GUID+2, 24, 4, 86901, 86873, 120, 1, 0, 0, 0);
 -- ---------------------------------
 -- Laser Decoration - left
 -- ---------------------------------
@@ -54,3 +115,19 @@ INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, 
 (@GUID+3, 11, 73689, 22, 4957,  -3312.7,        -885.5147,   -481.769,   -0.500161,  0, 0, 21396,  219, 219),
 (@GUID+4, 11, 73689, 22, 4957,  -3283.119,      -887.4646,   -482.6721,   0.8794132, 0, 0, 21396,  219, 219),
 (@GUID+5, 11, 73689, 51, 4959,          0,              1,           1,           1, 0, 0,     1,  219, 219);
+-- ---------------------------------
+-- In Game Store 86919 is SMC
+-- ---------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `Store_offer_item` (`Id`, `GroupId`, `Name`, `Description`, `DisplayFlags`, `Field_6`, `Field_7, `Visible`) VALUES
+(@GUID+1,                                     0,      0,             0,              0,         0,        0,        0);
+INSERT INTO `Store_category` (`Id`, `ParentId`, `Name`, `Description`, `Index`, `Visible`) VALUES
+(@GUID+1,                                    0,      0,             0,       0,        0);
+INSERT INTO `Store_offer_group` (`Id`, `DisplayFlags`, `Name`, `Description`, `DisplayInfoOverride`, `Visible`) VALUES
+(@GUID+1,                                           0,      0,             0,                     0,        0);
+INSERT INTO `Store_offer_category` (`Id`, `CategoryId`, `Index`, `Visible`) VALUES
+(@GUID+1,                                            0,       0,        0);
+INSERT INTO `Store_offer_data` (`Id`, `ItemId`, `Type`, `Amount`) VALUES
+(@GUID+1,                                86919,      0,       1);
+INSERT INTO `Store_offer_item_price` (`Id`, `CurrencyId`, `Price`, `DiscountType`, `DiscountValue`, `Field_14`, `Expiry`) VALUES
+(@GUID+1,                                      0,       0,              0,               0,          0,       0);
