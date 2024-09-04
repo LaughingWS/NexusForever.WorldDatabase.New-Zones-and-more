6885bc4488087af55a1641f89f1bc24f9c19fd71
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-09-04 03:22:57 -0400

    9/4/24

--- Events/Remove any event.sql
+++ Events/Remove any event.sql
@@ -2,29 +2,30 @@
 -- Live Event remover (WIP)
 -- Script written by Laughing
 -- --------------------------------------
--- Battle Chase (WIP)
+-- More then one event
+-- --------------------------------------
+DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (71788);
+DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2193 AND `Creature` IN (71788);
+-- --------------------------------------
+-- Battle Chase
 -- --------------------------------------
 DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (75133, 75135, 75131, 75132);
 DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (75133, 75135, 75131, 75132);
-DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2193 AND `Creature` IN (0);
 -- --------------------------------------
 -- Dungeon Chase
 -- --------------------------------------
-DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (71788, 73686, 73687, 73688, 73689);
+DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (73686, 73687, 73688, 73689);
 DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (73686, 73687, 73688, 73689);
-DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2193 AND `Creature` IN (71788);
 -- --------------------------------------
 -- Sim Chase
 -- --------------------------------------
-DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (71788, 73337, 73338, 73339, 73340);
+DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (73337, 73338, 73339, 73340);
 DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (73337, 73338, 73339, 73340);
-DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2193 AND `Creature` IN (71788);
 -- --------------------------------------
 -- Space Chase
 -- --------------------------------------
-DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (71788, 73165, 73166, 73211, 73212);
+DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (73165, 73166, 73211, 73212);
 DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (73165, 73166, 73211, 73212);
-DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2193 AND `Creature` IN (71788);
 -- --------------------------------------
 -- Residential Renovation (WIP)
 -- --------------------------------------
@@ -40,15 +41,16 @@ DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (0);
 -- --------------------------------------
 -- Starfall (WIP)
 -- --------------------------------------
-DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (73058, 71788, 74962, 74963, 74964, 74965);
-DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (73057, 74958, 74959, 74960, 74961);
-DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2193 AND `Creature` IN (71788);
+DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (73058, 73061, 74800, 74801, 74802, 74854, 74856, 74962, 74963, 74964, 74965);
+DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (73057, 73059, 73279, 73282, 73283, 74854, 74856, 74958, 74959, 74960, 74961);
 -- --------------------------------------
 -- zPrix (WIP)
 -- --------------------------------------
 DELETE FROM `entity` WHERE `world` = 51 AND `Creature` IN (71683, 71711, 71712, 71713, 71714, 71715, 71716, 71788, 71848, 71850, 71851, 71852, 71853, 71854, 71950, 71951, 71953, 71954, 71955, 71967, 71968, 71969, 72057, 72058, 74814, 74816, 74817);
-DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (71683, 71689, 71695, 71735, 71736, 71737, 71786, 71788, 71804, 71805, 71806, 71807, 71866, 71867, 71868, 71869, 71870, 71871, 71907, 71910, 71911, 71985, 72039, 72041, 72043, 72045, 72047, 72050, 72051, 72052, 72053, 72054, 72181, 74814, 74816, 74817);
-DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2193 AND `Creature` IN (71788);
+DELETE FROM `entity` WHERE `world` = 22 AND `Creature` IN (71683, 71689, 71695, 71735, 71736, 71737, 71786, 71804, 71805, 71806, 71807, 71866, 71867, 71868, 71869, 71870, 71871, 71907, 71910, 71911, 71985, 72039, 72041, 72043, 72045, 72047, 72050, 72051, 72052, 72053, 72054, 72181, 74814, 74816, 74817);
+DELETE FROM `entity` WHERE `world` = 22 AND `area` = 1 AND `Creature` IN (71788);
+DELETE FROM `entity` WHERE `world` = 22 AND `area` = 2 AND `Creature` IN (71788);
+DELETE FROM `entity` WHERE `world` = 51 AND `area` = 4959 AND `Creature` IN (71788); -- test other maps
 -- --------------------------------------
 -- Shade's Eve (WIP)
 -- --------------------------------------
