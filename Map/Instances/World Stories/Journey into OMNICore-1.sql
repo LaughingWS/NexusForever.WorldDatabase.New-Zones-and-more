20e5af9078a48ad20ee09f1ea2885b51be36756d
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-09-04 03:36:41 -0400

    9/4/24

--- Map/Instances/World Stories/Journey into OMNICore-1.sql
+++ Map/Instances/World Stories/Journey into OMNICore-1.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------
--- Jouney into OMNICore-1
+-- Journey into OMNICore-1
 -- --------------------------------------
 DELETE FROM map_entrance WHERE mapId = 3045;
 INSERT INTO map_entrance (mapId, team, worldLocationId) VALUES
