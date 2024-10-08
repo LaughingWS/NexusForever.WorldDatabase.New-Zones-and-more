e20a726f3f3ccacfc0f2ae0d4a29073eafebbd2a
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-10-08 06:20:34 -0400

    10/8/24

--- Map/Instances/Expeditions/Outpost M13.sql
+++ Map/Instances/Expeditions/Outpost M13.sql
@@ -1,6 +1,20 @@
 -- --------------------------------------
 -- Outpost M-13
 -- --------------------------------------
-DELETE FROM map_entrance WHERE mapId = 1319;
+SET @WORLD   = 1319;
+SET @EVENTID =  108;
+DELETE FROM map_entrance WHERE mapId = @WORLD;
 INSERT INTO map_entrance (mapId, team, worldLocationId) VALUE
-    (1319, 0, 16970);
+(@WORLD, 0, 16970);
+DELETE FROM `entity` WHERE `world` = @WORLD;
+-- --------------------------------------
+-- Captain Milo
+-- --------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 41201, @WORLD, 1540, 9791.18, -932.668, 5264.279, 0, 0, 0, 28578, 0, 466, 466);
+INSERT INTO `entity_stats` (`Id`, `Stat`, `Value`) VALUES
+(@GUID+1, 0, 1);
+INSERT INTO entity_event (id, eventId, phase) VALUES
+(@GUID+1, @EVENTID, 0);
+
