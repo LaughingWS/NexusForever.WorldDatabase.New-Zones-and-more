7bc8c62504dc1d7a28cba5974b7f1619edff3e1c
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-10-08 06:23:15 -0400

    10/8/24

--- Map/Instances/Dungeons/Skullcano.sql
+++ Map/Instances/Dungeons/Skullcano.sql
@@ -1,6 +1,17 @@
 -- --------------------------------------
--- Skullcano Dungeon
+-- Skullcano (Dungeon: Normal)
 -- --------------------------------------
-DELETE FROM map_entrance WHERE mapId = 1263;
+SET @WORLD = 1263;
+SET @EVENTID = 148;
+DELETE FROM map_entrance WHERE mapId = @WORLD;
 INSERT INTO map_entrance (mapId, team, worldLocationId) VALUES
-(1263, 0, 17726);
+(@WORLD, 0, 17726);
+DELETE FROM `entity` WHERE `world` = @WORLD;
+-- ---------------------------------------------
+-- Stew-Shaman Tugga - Normal
+-- ---------------------------------------------
+SET @GUID = (SELECT IFNULL(MAX(`id`), 0) FROM `entity`);
+INSERT INTO `entity` (`Id`, `Type`, `Creature`, `World`, `Area`, `X`, `Y`, `Z`, `RX`, `RY`, `RZ`, `DisplayInfo`, `OutfitInfo`, `Faction1`, `Faction2`) VALUES
+(@GUID+1, 0, 24493, @WORLD, 1220, 512.32, -979.10, -366.99, 0, 0, 0, 27916, 0, 868, 868);
+INSERT INTO entity_event (id, eventId, phase) VALUES
+(@GUID+1, @EVENTID, 0);
