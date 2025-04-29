97bb030bef5e04eb46193c4e405ab0398fba6efc
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2025-04-29 05:14:53 -0400

    Queue for some Adventures
    Adds notes about other spawns, and fixes up world locations

--- Map/Instances/Adventures/Crimelords of Whitevale.sql
+++ Map/Instances/Adventures/Crimelords of Whitevale.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------
--- Malgrave Trail (Adventure)
+-- Crimelords of Whitevale (Adventure)
 -- --------------------------------------
-SET @WORLD = 1181;
+SET @WORLD = 1323;
 DELETE FROM map_entrance WHERE mapId = @WORLD;
 INSERT INTO map_entrance (mapId, team, worldLocationId) VALUES
-(@WORLD, 0, 13662); -- Other team members 26842, 26843, 26844, 26845
+(@WORLD, 0, 32423); -- Other spots: 32422, 32424, 32425, 32426 |exile run on 5/30/2015
--- Map/Instances/Adventures/The Siege of Tempest Refuge.sql
+++ Map/Instances/Adventures/The Siege of Tempest Refuge.sql
@@ -4,4 +4,5 @@
 SET @WORLD = 1233;
 DELETE FROM map_entrance WHERE mapId = @WORLD;
 INSERT INTO map_entrance (mapId, team, worldLocationId) VALUES
-(@WORLD, 0, 15587);
+(@WORLD, 0, 37409); -- 37406?, 37410?, 37411?, 41495?| the coordinates shifted a bit from the videos I watched, but they are from 2015 
+                    -- Testing all the coordinates in 4123 shows me that this should be the new ones
